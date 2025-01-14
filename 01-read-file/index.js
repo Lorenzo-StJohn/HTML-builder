9a40d16203eeaf821a695bb7569d28857b1e56eb
Lorenzo-StJohn <britishaccent@bk.ru>
2025-01-15 02:25:28 +0400

    fix: add encoding type

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const textFile = path.join(__dirname, 'text.txt');
-const readStream = fs.createReadStream(textFile);
+const readStream = fs.createReadStream(textFile, 'utf-8');
 
 const standardOutputStream = (chunk) => {
   process.stdout.write(chunk.toString() + '\n');
