50ae4ac719b7ca5aa490aa6177628a99e0d287a2
Lorenzo-StJohn <britishaccent@bk.ru>
2025-01-16 06:12:43 +0400

    feat: implement writing to file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const textFile = path.join(__dirname, 'text.txt');
-const file = fs.createWriteStream(textFile);
+const writerStream = fs.createWriteStream(textFile);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const standardOutputStream = () => {
-  process.stdout.write('Type something here:\n');
+const startWriting = () => {
+  rl.write('Type something and press "Enter" after each line:\n');
+  rl.addListener('line', writeToFile);
 };
 
-file.addListener('open', standardOutputStream);
+const writeToFile = (line) => {
+  if (line.trim() === 'exit') {
+    finishWriting();
+  } else {
+    writerStream.write(line + '\n');
+  }
+};
+
+const finishWriting = () => {
+  rl.removeAllListeners('line');
+  rl.write('The task is completed. Have a nice day!\n');
+  writerStream.close();
+  rl.close();
+};
+
+writerStream.addListener('open', startWriting);
+rl.addListener('SIGINT', finishWriting);
