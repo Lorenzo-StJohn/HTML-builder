98efd879d1945552cffdb52e6a936c5f89d3676f
Lorenzo-StJohn <britishaccent@bk.ru>
2025-01-20 02:12:01 +0400

    fix: delete folder before creation

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,37 +6,20 @@ const filesCopyDir = path.join(__dirname, 'files-copy');
 
 async function copyDir(fromDir, toDir) {
   try {
+    await fsPromises.rm(toDir, {
+      recursive: true,
+      force: true,
+    });
     fsPromises.mkdir(toDir, { recursive: true });
     const copyFiles = await fsPromises.readdir(fromDir, {
       withFileTypes: true,
     });
-    const existedFiles = await fsPromises.readdir(toDir, {
-      withFileTypes: true,
-    });
-    const existedNames = [];
-    const copyNames = [];
-    for (const file of copyFiles) {
-      copyNames.push(file.name);
-    }
-    for (const file of existedFiles) {
-      if (!copyNames.includes(file.name)) {
-        if (file.isFile()) {
-          await fsPromises.unlink(path.join(toDir, file.name));
-        } else {
-          await fsPromises.rm(path.join(toDir, file.name), {
-            recursive: true,
-          });
-        }
-      } else existedNames.push(file.name);
-    }
     for (const file of copyFiles) {
       if (file.isFile()) {
-        if (!existedNames.includes(file.name)) {
-          await fsPromises.copyFile(
-            path.join(fromDir, file.name),
-            path.join(toDir, file.name),
-          );
-        }
+        await fsPromises.copyFile(
+          path.join(fromDir, file.name),
+          path.join(toDir, file.name),
+        );
       } else {
         copyDir(path.join(fromDir, file.name), path.join(toDir, file.name));
       }
