936a4425e64481f68e3481ee021d3fe8ab0ef21f
Lorenzo-StJohn <britishaccent@bk.ru>
2025-01-20 03:40:19 +0400

    feat: create folder for building page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,2 +1,9 @@
 const fsPromises = require('fs/promises');
 const path = require('path');
+const projectDistFolder = path.join(__dirname, 'project-dist');
+
+async function build(projectDist) {
+  await fsPromises.mkdir(projectDist, { recursive: true });
+}
+
+build(projectDistFolder);
